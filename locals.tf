97cfcd54cc4b88b00cdd5f4aa7e83c71393b0e0c
saidevops7722 <saiaws7722@gmail.com>
2024-03-09 15:06:52 +0530

    terraform infra

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
-local = {
+locals {
      ##private subnets
   db_subnet_ids = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id])
   app_subnet_ids = tolist([module.vpc["main"].private_subnets["app-az1"].id, module.vpc["main"].private_subnets["app-az2"].id])
