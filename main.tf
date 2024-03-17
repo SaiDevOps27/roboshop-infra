19c7a7bddd65ea1a78a69ddead755ee1b9621b2c
saidevops7722 <saiaws7722@gmail.com>
2024-03-17 11:07:16 +0530

    terraform infra

--- main.tf
+++ main.tf
@@ -13,62 +13,62 @@ module "vpc" {
 }
 
 
-#module "docdb" {
-#  source = "git::https://github.com/SaiDevOps27/tf-module-docdb.git"
-#  env = var.env
-#  tags = var.tags
-#
-#  for_each = var.docdb
-#  engine = each.value["engine"]
-#  backup_retention_period = each.value["backup_retention_period"]
-#  preferred_backup_window = each.value["preferred_backup_window"]
-#  skip_final_snapshot = each.value["skip_final_snapshot"]
-#  engine_version = each.value["engine_version"]
-#  no_of_instances = each.value["no_of_instances"]
-#  instance_class = each.value["instance_class"]
-#  subnet_ids = local.db_subnet_ids
-#}
+module "docdb" {
+  source = "git::https://github.com/SaiDevOps27/tf-module-docdb.git"
+  env = var.env
+  tags = var.tags
+
+  for_each = var.docdb
+  engine = each.value["engine"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot = each.value["skip_final_snapshot"]
+  engine_version = each.value["engine_version"]
+  no_of_instances = each.value["no_of_instances"]
+  instance_class = each.value["instance_class"]
+  subnet_ids = local.db_subnet_ids
+}
 
 
-#module "rds" {
-#  source = "git::https://github.com/SaiDevOps27/tf-module-rds.git"
-#  env = var.env
-#  tags = var.tags
-#
-#  for_each = var.rds
-#  engine = each.value["engine"]
-#  backup_retention_period = each.value["backup_retention_period"]
-#  preferred_backup_window = each.value["preferred_backup_window"]
-#  skip_final_snapshot = each.value["skip_final_snapshot"]
-#  engine_version = each.value["engine_version"]
-#  no_of_instances = each.value["no_of_instances"]
-#  instance_class = each.value["instance_class"]
-#  subnet_ids = local.db_subnet_ids
-#}
+module "rds" {
+  source = "git::https://github.com/SaiDevOps27/tf-module-rds.git"
+  env = var.env
+  tags = var.tags
 
-#module "elasticache" {
-#  source = "git::https://github.com/SaiDevOps27/tf-module-elasticache.git"
-#  env = var.env
-#  tags = var.tags
-#
-#  for_each = var.elasticache
-#  engine = each.value["engine"]
-#  engine_version = each.value["engine_version"]
-#  num_cache_nodes = each.value["num_cache_nodes"]
-#  node_type = each.value["node_type"]
-#  subnet_ids = local.db_subnet_ids
-#}
+  for_each = var.rds
+  engine = each.value["engine"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot = each.value["skip_final_snapshot"]
+  engine_version = each.value["engine_version"]
+  no_of_instances = each.value["no_of_instances"]
+  instance_class = each.value["instance_class"]
+  subnet_ids = local.db_subnet_ids
+}
+
+module "elasticache" {
+  source = "git::https://github.com/SaiDevOps27/tf-module-elasticache.git"
+  env = var.env
+  tags = var.tags
+
+  for_each = var.elasticache
+  engine = each.value["engine"]
+  engine_version = each.value["engine_version"]
+  num_cache_nodes = each.value["num_cache_nodes"]
+  node_type = each.value["node_type"]
+  subnet_ids = local.db_subnet_ids
+}
 
 
-#module "rabbitmq" {
-#  source = "git::https://github.com/SaiDevOps27/tf-module-rabbitmq.git"
-#  env = var.env
-#  tags = var.tags
-#
-#  for_each = var.rabbitmq
-#  instance_type = each.value["instance_type"]
-#  subnet_ids = local.db_subnet_ids
-#}
+module "rabbitmq" {
+  source = "git::https://github.com/SaiDevOps27/tf-module-rabbitmq.git"
+  env = var.env
+  tags = var.tags
+
+  for_each = var.rabbitmq
+  instance_type = each.value["instance_type"]
+  subnet_ids = local.db_subnet_ids
+}
 
 
 module "alb" {
