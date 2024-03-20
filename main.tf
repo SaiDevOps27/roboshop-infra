b5bf06a8ee22e0372aa1c0b536fefa1aa6b2edcf
saidevops7722 <saiaws7722@gmail.com>
2024-03-20 22:21:04 +0530

    terraform infra

--- main.tf
+++ main.tf
@@ -140,7 +140,7 @@ data "aws_ami" "ami" {
 resource "aws_spot_instance_request" "load-runner" {
 
   ami                    = data.aws_ami.ami.id
-  instance_type          = "t3.medium"
+  instance_type          = "t3.small"
   wait_for_fulfillment   = true
   vpc_security_group_ids = ["allow-all"]
 
