e764e508b0d77d9445b1f99e1898b4b9b6147546
saidevops7722 <saiaws7722@gmail.com>
2024-04-27 20:01:38 +0530

    jenkins provider info

--- data.tf
+++ data.tf
@@ -2,9 +2,9 @@ data "aws_ssm_parameter" "ssh_pass" {
   name = "${var.env}.ssh.pass"
 }
 
-#data "aws_ami" "ami" {
-#  most_recent = true
-#  name_regex = "devops-practice-with-ansible"
-#  owners = ["self"]
-#}
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "Centos-8-Devops-Practice"
+  owners = ["792438259559"]
+}
 
